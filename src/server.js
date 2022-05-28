25007ccd46660f92ae9e39d9052c5244c66ebea2
Higor Antonio dos Santos Benevenuto <higorantonio.ha@gmail.com>
2022-05-28 01:56:37 -0300

    primeiro commit

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const logger = require("./lib/logger");
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  logger.info(`Server running at port ${PORT}...`);
+});
